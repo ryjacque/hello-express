dc19204ce6138d41d0729f3af1d54b6a484f6209
Ryan Collins <ryanjcollins41@gmail.com>
2022-07-12 16:12:15 -0400

    final

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const Express = require("express");
 const app = Express();
 const PORT = 5000
+let message
 
 app.listen(PORT, ()=>{
     console.log(`You are listening on Port ${PORT}.`)
 })
-app.get("/", (req, res)=>{
-    console.log("<h1>this is home<h1>")
-})
+let pageParams = (req, res, next)=>{
+    message=(`You are on page: ${req.path}`)
+    next()
+}
+app.use(pageParams)
+
+
 app.get("/nerds", (req, res)=>{
-    res.send("<h3>You are nerd<h3>")
+    res.send(`<h1>${message}<h1>`)
     console.log("nerds")
+    
 })
 app.get("/ostrich", (req, res)=>{
-    res.send("<h1>You are ostrich<h1>")
+    res.send(`<h1>${message}<h1>`)
     console.log("ostrich")
 })
 app.get("/activat", (req, res)=>{
-    sessionStorage.send("<h1>YOU ARE NOW ACTIVATED</h1>");
+    sessionStorage.send(`<h1>${message}<h1>`);
     console.log("activated")
 })
+app.get( `/:key`, (req, res)=>{
+    res(`<h1>${message}<h1>`)
+})
+// if i navigate to localhost:5000.dog, it console.logs {key: dog}
+
+ 
